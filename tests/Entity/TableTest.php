2e7d3f3ea2f21f53859e7986554e3b511f48c5f1
Gleb Vasylenko <hleb.v@platinumlist.pro>
2020-09-03 13:37:25 +0300

    TEST: add tests for intersectWith method of Reserve entity

--- tests/Entity/TableTest.php
+++ tests/Entity/TableTest.php
@@ -74,4 +74,52 @@ class TableTest extends TestCase
         $reserves = $table->getReserves();
         $this->assertSame(0, $reserves->count());
     }
+
+    public function test_intersectWith_noIntersect()
+    {
+        $hall = new Hall('Main');
+        $table = new Table($hall, 1);
+        $reserve1 = new Reserve(
+            $table, 
+            new ReserveInterval(
+                new DateTime('2020-08-12 18:00:00'),
+                new DateTime('2020-08-12 20:00:00')
+            )
+        );
+
+        $reserve2 = new Reserve(
+            $table, 
+            new ReserveInterval(
+                new DateTime('2020-08-12 20:00:00'),
+                new DateTime('2020-08-12 21:00:00')
+            )
+        );
+
+        $this->assertFalse($reserve1->intersectWith($reserve2));
+        $this->assertFalse($reserve2->intersectWith($reserve1));
+    }
+
+    public function test_intersectWith_intersect()
+    {
+        $hall = new Hall('Main');
+        $table = new Table($hall, 1);
+        $reserve1 = new Reserve(
+            $table, 
+            new ReserveInterval(
+                new DateTime('2020-08-12 18:00:00'),
+                new DateTime('2020-08-12 20:00:00')
+            )
+        );
+
+        $reserve2 = new Reserve(
+            $table, 
+            new ReserveInterval(
+                new DateTime('2020-08-12 17:30:00'),
+                new DateTime('2020-08-12 18:30:00')
+            )
+        );
+
+        $this->assertTrue($reserve1->intersectWith($reserve2));
+        $this->assertTrue($reserve2->intersectWith($reserve1));
+    }
 }
