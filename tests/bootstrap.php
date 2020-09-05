6dcfc0b3000c4de5032adaa6530b41bef52d0dab
glebvvss <glebvvss@ukr.net>
2020-09-05 12:40:47 +0300

    TEST: Mockery phpunit verify trait

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,3 +9,20 @@ if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
+
+trait MockeryAssertions
+{
+    public function assertMockerySpy($mock)
+    {
+        try {
+            $mock->verify();
+            $result = true;
+        } catch (Throwable $ex) {
+            $result = false;
+        }
+
+        $this->assertTrue($result);
+    }
+
+    abstract public function assertTrue($assertion);
+}
