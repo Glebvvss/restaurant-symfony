72636a18e308728c83e29c9443f55a845ac2eefe
glebvvss <glebvvss@ukr.net>
2020-09-05 12:43:00 +0300

    TEST: Hall actions test uses Mockery

--- tests/Action/Hall/CreateActionTest.php
+++ tests/Action/Hall/CreateActionTest.php
@@ -2,28 +2,61 @@
 
 namespace App\Test\Action\Hall;
 
+use Mockery;
+use App\Entity\Hall;
+use MockeryAssertions;
 use Doctrine\ORM\EntityManager;
 use PHPUnit\Framework\TestCase;
+use App\Exception\ErrorReporting;
 use App\Action\Hall\CreateAction;
+use App\Repository\HallRepository;
 
 class CreateActionTest extends TestCase
 {
-    private $action;
+    use MockeryAssertions;
 
-    public function setUp()
+    public function test_handle_success()
     {
-        $em = $this->createMock(EntityManager::class);
-        $this->action = new CreateAction($em);
-    }
+        $repository = Mockery::spy(HallRepository::class);
+        $repository->shouldReceive('nameReserved')
+                   ->with('Main')
+                   ->andReturn(false);
 
-    public function test_handle()
-    {
+        $em = Mockery::spy(EntityManager::class);
+        $em->shouldReceive('getRepository')
+           ->with(Hall::class)
+           ->andReturn($repository);
+
+        $em->shouldReceive('persist');
+        $em->shouldReceive('flush');
+
+        $action = new CreateAction($em);
         $this->assertEquals(
             [
                 'id'   => null,
                 'name' => 'Main'
             ],
-            $this->action->handle('Main')
+            $action->handle('Main')
         );
+
+        $this->assertMockerySpy($em);
+    }
+
+    public function test_handle_failed()
+    {
+        $this->expectException(ErrorReporting::class);
+
+        $repository = Mockery::spy(HallRepository::class);
+        $repository->shouldReceive('nameReserved')
+                   ->with('Main')
+                   ->andReturn(true);
+
+        $em = Mockery::spy(EntityManager::class);
+        $em->shouldReceive('getRepository')
+           ->with(Hall::class)
+           ->andReturn($repository);
+
+        $action = new CreateAction($em);
+        $action->handle('Main');
     }
 }
---
+++ tests/Action/Hall/DeleteActionTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Test\Action\Hall;
+
+use Mockery;
+use App\Entity\Hall;
+use ReflectionClass;
+use MockeryAssertions;
+use Doctrine\ORM\EntityManager;
+use PHPUnit\Framework\TestCase;
+use App\Action\Hall\DeleteAction;
+
+class DeleteActionTest extends TestCase
+{
+    use MockeryAssertions;
+
+    public function test_handle()
+    {
+        $class = new ReflectionClass(Hall::class);
+        $property = $class->getProperty("id");
+        $property->setAccessible(true);
+
+        $hall = new Hall('Main');
+        $property->setValue($hall, 1);
+
+        $repository = Mockery::mock(EntityManager::class);
+        $repository->shouldReceive('findOne')
+                   ->with(1)
+                   ->once()
+                   ->andReturn($hall);
+
+        $em = Mockery::spy(EntityManager::class);
+        $em->shouldReceive('getRepository')
+           ->with(Hall::class)
+           ->once()
+           ->andReturn($repository);
+
+        $action = new DeleteAction($em);
+        $action->handle(1);
+
+        $this->assertMockerySpy($em);
+    }
+}
