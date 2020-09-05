77a44ce23ab308dcc9103c918085ba31fd5b0678
glebvvss <glebvvss@ukr.net>
2020-09-05 19:17:20 +0300

    REFACTORING: Move reserve entity tests from table test class to reserve test class

--- tests/Entity/ReserveTest.php
+++ tests/Entity/ReserveTest.php
@@ -43,4 +43,44 @@ class ReserveTest extends TestCase
         $this->assertSame($timeFrom, $reserve->getTimeFrom());
         $this->assertSame($timeTo,   $reserve->getTimeTo());
     }
+
+    public function test_intersectWith_noIntersect()
+    {
+        $reserve1 = new Reserve(
+            new ReserveInterval(
+                new DateTime('2020-08-12 18:00:00'),
+                new DateTime('2020-08-12 20:00:00')
+            )
+        );
+
+        $reserve2 = new Reserve(
+            new ReserveInterval(
+                new DateTime('2020-08-12 20:00:00'),
+                new DateTime('2020-08-12 21:00:00')
+            )
+        );
+
+        $this->assertFalse($reserve1->intersectWith($reserve2));
+        $this->assertFalse($reserve2->intersectWith($reserve1));
+    }
+
+    public function test_intersectWith_intersect()
+    {
+        $reserve1 = new Reserve(
+            new ReserveInterval(
+                new DateTime('2020-08-12 18:00:00'),
+                new DateTime('2020-08-12 20:00:00')
+            )
+        );
+
+        $reserve2 = new Reserve(
+            new ReserveInterval(
+                new DateTime('2020-08-12 17:30:00'),
+                new DateTime('2020-08-12 18:30:00')
+            )
+        );
+
+        $this->assertTrue($reserve1->intersectWith($reserve2));
+        $this->assertTrue($reserve2->intersectWith($reserve1));
+    }
 }
--- tests/Entity/TableTest.php
+++ tests/Entity/TableTest.php
@@ -66,46 +66,4 @@ class TableTest extends TestCase
         $reserves = $table->getReserves();
         $this->assertSame(0, $reserves->count());
     }
-
-    public function test_intersectWith_noIntersect()
-    {
-        $table = new Table(1);
-        $reserve1 = new Reserve(
-            new ReserveInterval(
-                new DateTime('2020-08-12 18:00:00'),
-                new DateTime('2020-08-12 20:00:00')
-            )
-        );
-
-        $reserve2 = new Reserve(
-            new ReserveInterval(
-                new DateTime('2020-08-12 20:00:00'),
-                new DateTime('2020-08-12 21:00:00')
-            )
-        );
-
-        $this->assertFalse($reserve1->intersectWith($reserve2));
-        $this->assertFalse($reserve2->intersectWith($reserve1));
-    }
-
-    public function test_intersectWith_intersect()
-    {
-        $table = new Table(1);
-        $reserve1 = new Reserve(
-            new ReserveInterval(
-                new DateTime('2020-08-12 18:00:00'),
-                new DateTime('2020-08-12 20:00:00')
-            )
-        );
-
-        $reserve2 = new Reserve(
-            new ReserveInterval(
-                new DateTime('2020-08-12 17:30:00'),
-                new DateTime('2020-08-12 18:30:00')
-            )
-        );
-
-        $this->assertTrue($reserve1->intersectWith($reserve2));
-        $this->assertTrue($reserve2->intersectWith($reserve1));
-    }
 }
